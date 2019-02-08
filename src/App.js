72d86fa8db242b30186c0801df780a7ba4f80a86
JJ <jj.spetseris@yahoo.com>
2019-02-07 20:12:52 -0600

    styling and clear button added

--- src/App.js
+++ src/App.js
@@ -17,9 +17,9 @@ class App extends Component {
     this.handleNameChange = this.handleNameChange.bind(this);
     this.generateNameTags = this.generateNameTags.bind(this);
     this.selectNewLanguage = this.selectNewLanguage.bind(this);
+    this.clearAll = this.clearAll.bind(this);
   }
-  generateNameTags() {
-    // console.log(this.state.names)
+  generateGreeting() {
     let greeting
     for (let i = 0; i < this.state.data.length; i++) {
       if (this.state.data[i].language === this.state.selectedLanguage) {
@@ -27,7 +27,11 @@ class App extends Component {
         break
       }
     }
+    return greeting
+  }
+  generateNameTags() {
     const tags = this.state.names
+    const greeting = this.generateGreeting()
     this.setState({tags: tags, greeting: greeting})
   }
   selectNewLanguage(event) {
@@ -36,32 +40,41 @@ class App extends Component {
   handleNameChange(event) {
     this.setState({nameText: event.target.value, names: event.target.value.split("\n")})
   }
+  clearAll() {
+    this.setState({tags: null, nameText: "", names: []})
+  }
   select
   render() {
     return (
-      <div className="App">
-        <br className="mb1"></br>
-        Select a Language: <select
-          className="mb1"
-          onChange={this.selectNewLanguage}
-          value={this.state.selectedLanguage}
-        >
-          {this.state.data.map((obj) => (
-            <option>{obj.language}</option>
-          ))}
-        </select>
+      <div className="App m2 container">
+        <div className="row">
+          <div className="col">
+            <textarea className="text-area" name="names"
+              onChange={this.handleNameChange}
+              value={this.state.nameText}/>
+          </div>
+          <div className="col text-area-right">
+            <div>
+              Select a Language: <select
+                className="mb1"
+                onChange={this.selectNewLanguage}
+                value={this.state.selectedLanguage}
+              >
+                {this.state.data.map((obj) => (
+                  <option>{obj.language}</option>
+                ))}
+              </select>
+            </div>
+            <Button className="mb1 mr1" onClick={ this.generateNameTags } variant="info">Generate Name Tags</Button>
+            <Button className="mb1" onClick={ this.clearAll } variant="secondary">Clear All</Button>
+          </div>
+        </div>
 
-        <br className="mb1"></br>
-        <textarea name="names"
-          onChange={this.handleNameChange}
-          value={this.state.nameText}/>
-        <br className="mb1"></br>
-        <Button className="mb1" onClick={ this.generateNameTags } variant="primary">Generate Name Tags</Button>
-        {this.state.tags ? <div>
+        {this.state.tags ? <div className="container">
             {this.state.tags.map( tag => (
               <div className="tag">
-                <div>{this.state.greeting}</div>
-                <div>{tag}</div>
+                <div><b>{this.state.greeting}</b></div>
+                <div className="underline">{tag}</div>
               </div>
             ))}
         </div> : null}
